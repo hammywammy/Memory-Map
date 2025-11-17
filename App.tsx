3b5a6b01e770624b8db6842f3d1b987dadf094b2
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-11-16 19:03:10 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import CanvasScreen from './src/screens/CanvasScreen';
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
-        <StatusBar barStyle="light-content" />
-        <CanvasScreen />
-      </SafeAreaView>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+          <StatusBar barStyle="light-content" />
+          <CanvasScreen />
+        </SafeAreaView>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
