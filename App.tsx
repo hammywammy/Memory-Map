c1054eb6cb1e3030e7bb9d930ff361ce9ec57b39
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-11-16 16:41:38 -0800

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SimulationScreen from './src/screens/SimulationScreen';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+        <StatusBar barStyle="light-content" />
+        <SimulationScreen />
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+}
